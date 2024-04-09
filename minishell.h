e672f8128b8cc4c710f84558f8879a20e438bf74
Tulece <antoine.porceddu@hotmail.ca>
2024-04-09 16:30:10 +0200

    commentaires .h

--- minishell.h
+++ minishell.h
@@ -27,11 +27,11 @@ typedef struct s_token
 	struct s_token	*next;
 }	t_token;
 
-int			ft_isvalidchar(char c);
+// utils
 void		swap_char(char *c1, char *c2);
 void		revstr(char *str);
-void		parse_input(char *input, t_token **token_lst);
 
+// lexing
 void		lexer(char	*input, t_token **tokens);
 t_token		*token_new(t_token_type type, void *content);
 t_token 	*token_last(t_token *token_lst);
@@ -39,14 +39,18 @@ void		tokenaddback(t_token **lst, t_token *node);
 void		treat_token(char *token, t_token **token_lst);
 void		print_token_type(t_token *token);
 
+// parsing
 void		parser(t_token *tokens);
+int			ft_isvalidchar(char c);
 void		parse_input(char *input, t_token **token_lst);
 
-
+// signal
 void		setup_signal_handlers(void);
 void		handle_sigint(int sig);
 
+// execution (faut la faire mdr)
 
+// memory
 void		free_tokens(t_token	*token);
 
 #endif
