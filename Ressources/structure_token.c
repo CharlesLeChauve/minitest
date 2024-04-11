7359391e7490fa80387130043df15bd34029bf6e
Tulece <antoine.porceddu@hotmail.ca>
2024-04-11 15:45:51 +0200

    structure token

--- Ressources/structure_token.c
+++ Ressources/structure_token.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 // Définition des différents types de tokens pouvant être rencontrés.
-typedef enum {
+typedef enum
+{
     COMMAND, ARGUMENT, PIPE, REDIRECT_IN, REDIRECT_OUT, REDIRECT_APPEND, BACKGROUND, ENV_VAR, EOL, EOF
 } TokenType;
 
 // Variable globale contenant le token courant.
 TokenType currentToken;
 
-// Prototypes des fonctions.
-void nextToken(void);
-void error(const char message[]);
-
 // Fonction pour accepter le token attendu et passer au suivant.
-int accept(TokenType expectedToken) {
-    if (currentToken == expectedToken) {
+int accept(TokenType expectedToken)
+{
+    if (currentToken == expectedToken)
+	{
         nextToken();
         return 1;
     }
@@ -22,7 +21,8 @@ int accept(TokenType expectedToken) {
 }
 
 // Fonction pour s'attendre à un token spécifique et avancer; sinon, erreur.
-int expect(TokenType expectedToken) {
+int expect(TokenType expectedToken)
+{
     if (accept(expectedToken))
         return 1;
     error("Token inattendu");
@@ -30,55 +30,68 @@ int expect(TokenType expectedToken) {
 }
 
 // Parsing d'une commande.
-void command(void) {
+void command(void)
+{
     expect(COMMAND);
-    while (accept(ARGUMENT)) {
+    while (accept(ARGUMENT))
+	{
         // Boucle pour gérer tous les arguments d'une commande.
     }
 }
 
 // Parsing d'une ligne de commande.
-void commandLine(void) {
+void commandLine(void)
+{
     command();
-    while (currentToken == PIPE) {
+    while (currentToken == PIPE)
+	{
         nextToken();
         command();
     }
-    if (accept(REDIRECT_OUT)) {
+    if (accept(REDIRECT_OUT))
+	{
         expect(ARGUMENT); // Nom du fichier pour la redirection.
     }
     // Ajouter ici d'autres cas de parsing comme les redirections et les variables d'environnement.
 }
 
 // Démarrage du parsing.
-void startParsing(void) {
+void startParsing(void)
+{
     nextToken(); // Initialiser avec le premier token.
     commandLine();
     expect(EOL); // Une commande se termine par une fin de ligne.
 }
 
-void nextToken(void) {
+void nextToken(void)
+{
     // Exemple fictif de la façon dont on pourrait déterminer le prochain token.
     // Dans une implémentation réelle, cela dépendrait de l'analyse de l'entrée utilisateur.
 
     char *nextInput = getNextInput(); // Suppose une fonction qui lit le prochain élément de l'entrée.
 
-    if (strcmp(nextInput, "|") == 0) {
+    if (strcmp(nextInput, "|") == 0)
+	{
         currentToken = PIPE;
-    } else if (isCommand(nextInput)) { // Suppose une fonction qui vérifie si l'entrée est une commande.
+    } else if (isCommand(nextInput))
+	{ // Suppose une fonction qui vérifie si l'entrée est une commande.
         currentToken = COMMAND;
-    } else if (isArgument(nextInput)) { // Suppose une fonction qui vérifie si l'entrée est un argument.
+    } else if (isArgument(nextInput))
+	{ // Suppose une fonction qui vérifie si l'entrée est un argument.
         currentToken = ARGUMENT;
-    } else if (strcmp(nextInput, ">") == 0) {
+    } else if (strcmp(nextInput, ">") == 0)
+	{
         currentToken = REDIRECT_OUT;
     }
     // Continuer avec les autres types de tokens...
-    else {
+    else
+	{
         currentToken = EOL; // Indique la fin de la ligne/commande.
     }
 }
 
-void error(const char message[]) {
+void error(const char message[])
+{
     fprintf(stderr, "Erreur : %s\n", message);
     // Ici, vous pourriez choisir de nettoyer, de demander une nouvelle entrée, ou d'arrêter le shell.
     exit(EXIT_FAILURE); // Sortie du programme avec une erreur.
