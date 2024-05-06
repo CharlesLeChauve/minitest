460d3d85b9f744a7211f40c8b1769410b10e863c
Tulece <antoine.porceddu@hotmail.ca>
2024-05-06 16:56:03 +0200

    worst norm ever but is ok

--- parserv4.c
+++ parserv4.c
@@ -7,55 +7,62 @@ int	ft_isshelloperator(char c)
 	return (0);
 }
 
-void ft_add_char_to_buffer(char **buffer, char c, size_t *len, t_tkn_info *tkn_info)
+void	ft_add_char_to_buffer(char **buffer, char c, size_t *len,
+		t_tkn_info *tkn_info)
 {
-    char *new_buffer;
-    if ((tkn_info->state == quote && c == '\'' && *len > 0 && (*buffer)[*len - 1] != '\\') ||
-        (tkn_info->state == dquote && c == '"' && *len > 0 && (*buffer)[*len - 1] != '\\'))
-    {
-        tkn_info->state = reg;
-        return;
-    }
-    new_buffer = malloc(*len + 2);
-    if (!new_buffer)
-    {
-        ft_printf("Error: Allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-    if (*buffer) ft_memcpy(new_buffer, *buffer, *len);
-    new_buffer[*len] = c;
-    (*len)++;
-    new_buffer[*len] = '\0';
-    free(*buffer);
-    *buffer = new_buffer;
+	char	*new_buffer;
+
+	if ((tkn_info->state == quote && c == '\'' \
+		&& *len > 0 && (*buffer)[*len - 1] != '\\') \
+		|| (tkn_info->state == dquote && c == '"' && *len > 0 \
+		&& (*buffer)[*len - 1] != '\\'))
+	{
+		tkn_info->state = reg;
+		return ;
+	}
+	new_buffer = malloc(*len + 2);
+	if (!new_buffer)
+	{
+		ft_printf("Error: Allocation failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if (*buffer)
+		ft_memcpy(new_buffer, *buffer, *len);
+	new_buffer[*len] = c;
+	(*len)++;
+	new_buffer[*len] = '\0';
+	free(*buffer);
+	*buffer = new_buffer;
 }
 
-
-
-void set_quotes_state(t_tkn_info *tkn_info)
+void	set_quotes_state(t_tkn_info *tkn_info)
 {
-    if (*tkn_info->curr_char == '\'' && (tkn_info->curr_char == tkn_info->input || *(tkn_info->curr_char - 1) != '\\'))
-    {
-        if (tkn_info->state == reg || tkn_info->state == dquote) // Si le précédent état était régulier ou guillemets doubles, passe à l'état des guillemets simples
-            tkn_info->state = quote;
-        else if (tkn_info->state == quote && *(tkn_info->curr_char + 1) == '\'') // Si le précédent état était guillemets simples et le prochain caractère est une apostrophe, reste dans l'état des guillemets simples
-            tkn_info->curr_char++; // Avancez pour ignorer l'apostrophe suivante
-        else // Sinon, retourne à l'état régulier
-            tkn_info->state = reg;
-        tkn_info->curr_char++; // Avancez uniquement après avoir changé l'état
-    }
-    else if (*tkn_info->curr_char == '"' && (tkn_info->curr_char == tkn_info->input || *(tkn_info->curr_char - 1) != '\\'))
-    {
-        if (tkn_info->state == reg || tkn_info->state == quote) // Si le précédent état était régulier ou guillemets simples, passe à l'état des guillemets doubles
-            tkn_info->state = dquote;
-        else if (tkn_info->state == dquote && *(tkn_info->curr_char + 1) == '"') // Si le précédent état était guillemets doubles et le prochain caractère est un guillemet, reste dans l'état des guillemets doubles
-            tkn_info->curr_char++; // Avancez pour ignorer le guillemet suivant
-        else // Sinon, retourne à l'état régulier
-            tkn_info->state = reg;
-        tkn_info->curr_char++; // Avancez uniquement après avoir changé l'état
-    }
+	if (*tkn_info->curr_char == '\'' \
+		&& (tkn_info->curr_char == tkn_info->input \
+		|| *(tkn_info->curr_char - 1) != '\\'))
+	{
+		if (tkn_info->state == reg || tkn_info->state == dquote)
+			tkn_info->state = quote;
+		else if (tkn_info->state == quote \
+				&& *(tkn_info->curr_char + 1) == '\'')
+			tkn_info->curr_char++;
+		else
+			tkn_info->state = reg;
+		tkn_info->curr_char++;
+	}
+	else if (*tkn_info->curr_char == '"' \
+			&& (tkn_info->curr_char == tkn_info->input \
+			|| *(tkn_info->curr_char - 1) != '\\'))
+	{
+		if (tkn_info->state == reg || tkn_info->state == quote)
+			tkn_info->state = dquote;
+		else if (tkn_info->state == dquote && *(tkn_info->curr_char + 1) == '"')
+			tkn_info->curr_char++;
+		else
+			tkn_info->state = reg;
+		tkn_info->curr_char++;
+	}
 }
-
 
 void	space_quotes(t_tkn_info *tkn_info)
 {
@@ -72,47 +79,39 @@ int	break_token(t_tkn_info *tkn_info, int is_redir)
 	state = tkn_info->state;
 	if (state == reg && (ft_isshelloperator(*tkn_info->curr_char)))
 		return (1);
-
-	else if ((state == quote && *tkn_info->curr_char == '\'') || (state == dquote && *tkn_info->curr_char == '"') && is_redir)
+	else if ((state == quote && *tkn_info->curr_char == '\'') \
+			|| (state == dquote && *tkn_info->curr_char == '"') && is_redir)
 		return (1);
 	else
 		return (0);
 }
 
-void set_token_text(t_tkn_info *tkn_info, t_token_lst *token)
+void	set_token_text(t_tkn_info *tkn_info, t_token_lst *token)
 {
-    size_t len = 0;
-    char *buffer = NULL;
-    while (*tkn_info->curr_char)
-    {
-        if (break_token(tkn_info, 0))
-            break;
-        // if (ft_isspace(*tkn_info->curr_char))
-        // {
-		// 	space_quotes(tkn_info);
-        //     char *peek = tkn_info->curr_char + 1;
-        //     while (ft_isspace(*peek))
-		// 		peek++;
-        //     if ((ft_isshelloperator(*peek) || *peek == '|' || *peek == '\0' || *peek == '\n') && tkn_info->state == reg)
-        //         break;
-        // }
+	size_t		len;
+	char		*buffer;
+
+	len = 0;
+	buffer = NULL;
+	while (*tkn_info->curr_char)
+	{
+		if (break_token(tkn_info, 0))
+			break ;
 		if (*tkn_info->curr_char == '"' || *tkn_info->curr_char == '\'')
 		{
 			set_quotes_state(tkn_info);
-
-			continue;
+			continue ;
 		}
-        ft_add_char_to_buffer(&buffer, *tkn_info->curr_char, &len, tkn_info);
-        tkn_info->curr_char++;
-    }
-    while (len > 0 && ft_isspace(buffer[len - 1]))
-    {
-        buffer[len - 1] = '\0';
-        len--;
-    }
-    token->text = buffer;  // Assurez-vous que buffer n'est pas NULL avant de l'utiliser
+		ft_add_char_to_buffer(&buffer, *tkn_info->curr_char, &len, tkn_info);
+		tkn_info->curr_char++;
+	}
+	while (len > 0 && ft_isspace(buffer[len - 1]))
+	{
+		buffer[len - 1] = '\0';
+		len--;
+	}
+	token->text = buffer;
 }
-
 
 t_token_lst	*redir_token(t_tkn_info *tkn_info)
 {
@@ -174,7 +173,7 @@ t_token_lst	*redir_token(t_tkn_info *tkn_info)
 
 t_token_lst	*cmd_token(t_tkn_info *tkn_info)
 {
-	t_token_lst *token;
+	t_token_lst	*token;
 
 	token = (t_token_lst *)malloc(sizeof(t_token_lst));
 	token->type = command;
@@ -182,51 +181,49 @@ t_token_lst	*cmd_token(t_tkn_info *tkn_info)
 	return (token);
 }
 
-
-t_token_lst *next_token(t_tkn_info *tkn_info)
+t_token_lst	*next_token(t_tkn_info *tkn_info)
 {
-    space_quotes(tkn_info);
-    if (!*tkn_info->curr_char)  // Plus sécurisé pour les chaînes vides
-        return token_new(eol, NULL);
-    if (*tkn_info->curr_char == '|') {
-        tkn_info->curr_char++;
-        if (*tkn_info->curr_char == '|') {
-            tkn_info->curr_char++;
-            return token_new(or_op, strdup("||"));
-        }
-        return token_new(pipe_op, strdup("|"));
-    } else if (*tkn_info->curr_char == '&' && *(tkn_info->curr_char + 1) == '&') {
-        tkn_info->curr_char += 2;
-        return token_new(and_op, strdup("&&"));
-    }
-    if ((*tkn_info->curr_char == '>' || *tkn_info->curr_char == '<') && tkn_info->state == reg)
-        return redir_token(tkn_info);
-
-    return cmd_token(tkn_info);
+	space_quotes(tkn_info);
+	if (!*tkn_info->curr_char)
+		return (token_new(eol, NULL));
+	if (*tkn_info->curr_char == '|')
+	{
+		tkn_info->curr_char++;
+		if (*tkn_info->curr_char == '|')
+		{
+			tkn_info->curr_char++;
+			return (token_new(or_op, ft_strdup("||")));
+		}
+		return (token_new(pipe_op, ft_strdup("|")));
+	}
+	else if (*tkn_info->curr_char == '&' && *(tkn_info->curr_char + 1) == '&')
+	{
+		tkn_info->curr_char += 2;
+		return (token_new(and_op, ft_strdup("&&")));
+	}
+	if ((*tkn_info->curr_char == '>' || *tkn_info->curr_char == '<') \
+		&& tkn_info->state == reg)
+		return (redir_token(tkn_info));
+	return (cmd_token(tkn_info));
 }
 
-
-
-t_dlist *tokenize(char *input)
+t_dlist	*tokenize(char *input)
 {
-    if (!input || !*input) {  // Vérifiez si l'input est NULL ou vide
-        return NULL;
-    }
+	t_dlist		*last;
+	t_tkn_info	tkn_info;
 
-    t_dlist *last;
-    t_tkn_info tkn_info;
-
-    tkn_info.input = ft_strdup(input);
-    tkn_info.curr_char = tkn_info.input;  // Doit pointer sur la copie, pas sur l'original
-    tkn_info.token_lst = NULL;
-    tkn_info.state = reg;
-    ft_dlstadd_back(&tkn_info.token_lst, ft_dlstnew(next_token(&tkn_info)));
-    last = (t_dlist *)ft_lstlast((t_list *)tkn_info.token_lst);
-    while (last && ((t_token_lst *)(last->content))->type != eol)
-    {
-        ft_dlstadd_back(&tkn_info.token_lst, ft_dlstnew(next_token(&tkn_info)));
-        last = (t_dlist *)ft_lstlast((t_list *)tkn_info.token_lst);
-    }
-    return tkn_info.token_lst;
+	if (!input || !*input)
+		return (NULL);
+	tkn_info.input = ft_strdup(input);
+	tkn_info.curr_char = tkn_info.input;
+	tkn_info.token_lst = NULL;
+	tkn_info.state = reg;
+	ft_dlstadd_back(&tkn_info.token_lst, ft_dlstnew(next_token(&tkn_info)));
+	last = (t_dlist *)ft_lstlast((t_list *)tkn_info.token_lst);
+	while (last && ((t_token_lst *)(last->content))->type != eol)
+	{
+		ft_dlstadd_back(&tkn_info.token_lst, ft_dlstnew(next_token(&tkn_info)));
+		last = (t_dlist *)ft_lstlast((t_list *)tkn_info.token_lst);
+	}
+	return (tkn_info.token_lst);
 }
-
--- utils_check.c
+++ utils_check.c
@@ -32,14 +32,24 @@ int	verify_tokens(t_dlist *tokens)
 	{
 		token = (t_token_lst *)current->content;
 		if (is_operator(token->type))
-			if (!prev || !current->next || (!is_command(((t_token_lst *)prev->content)->type) && !is_redir(((t_token_lst *)prev->content)->type)) || !is_command(((t_token_lst *)current->next->content)->type))
-				return (fprintf(stderr, "Error: Operator '%s' not between two commands\n", token->text), 0);
+			if (!prev || !current->next \
+				|| (!is_command(((t_token_lst *)prev->content)->type) \
+				&& !is_redir(((t_token_lst *)prev->content)->type)) \
+				|| !is_command(((t_token_lst *)current->next->content)->type))
+				return (fprintf(stderr, "Error: Operator '%s' not between two \
+commands\n", token->text), 0);
 		if (is_redir(token->type))
 			if (!current->next)
-				return (fprintf(stderr, "Error: Redirection '%s' not followed by a command\n", token->text), 0);
-		if (current->next && is_operator(((t_token_lst *)current->next->content)->type) && ((t_token_lst *)current->next->content)->type == pipe_op)
-			if (!current->next->next || !is_command(((t_token_lst *)current->next->next->content)->type))
-				return (fprintf(stderr, "Error: Redirection '%s' not followed by a command\n", token->text), 0);
+				return (fprintf(stderr, "Error: Redirection '%s' not followed \
+by a command\n", token->text), 0);
+		if (current->next \
+			&& is_operator(((t_token_lst *)current->next->content)->type) \
+			&& ((t_token_lst *)current->next->content)->type == pipe_op)
+			if (!current->next->next \
+				|| !is_command(((t_token_lst *) \
+				current->next->next->content)->type))
+				return (fprintf(stderr, "Error: Redirection '%s' not followed \
+by a command\n", token->text), 0);
 		prev = current;
 		current = current->next;
 	}
