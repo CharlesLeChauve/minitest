5c3c6d1c68288ce3b8e1e32aa71a5021aaabe18c
CharlesLeChauve <teddy.gibert@gmail.com>
2024-06-27 14:32:42 +0200

    fixed export errors and leaks

--- export.c
+++ export.c
@@ -46,42 +46,107 @@ char	*export_format(char *str)
 
 int	check_export_arg(char *arg)
 {
-	int	ret;
-	int	i;
+	int		i;
+	int		alpha;
+	char	err[128];
 
 	i = 0;
+	alpha = 0;
 	while (arg[i] && arg[i] != '=')
 	{
-		if (arg[i])
+		if ((i == 0 && arg[i] == '=') || (!ft_isalnum(arg[i]) && arg[i] != '_') || (alpha == 0 && ft_isdigit(arg[i])))
+		{
+			ft_sprintf(err, "tash: export: '%s': not a valid identifier", arg[i]);
+			ft_putstr_fd(err, STDERR_FILENO);
+			return (1);
+		}
+		if (ft_isalpha(arg[i]))
+			alpha = 1;
+		i++;
 	}
+	return (0);
 }
+
+char **build_export_env(char **env[])
+{
+	int		i;
+	char	**env_cpy;
+
+	i = 0;
+	while ((*env)[i])
+		i++;
+	env_cpy = (char **)malloc((i + 1) * sizeof(char *));
+	i = -1;
+	while ((*env)[++i])
+		env_cpy[i] = export_format((*env)[i]);
+	env_cpy[i] = NULL;
+	return (env_cpy);
+}
+
+void	add_str_to_strtab(char *str, char ***strtab)
+{
+	int	env_size;
+	int	total_size;
+
+	env_size = 0;
+	while ((*strtab)[env_size])
+		env_size++;
+	total_size = env_size + 1;
+	*strtab = ft_realloc(*strtab, env_size * sizeof(char *), (total_size + 1) * sizeof(char *));
+	if (*strtab == NULL)
+		return ;
+	(*strtab)[env_size] = str;
+	if ((*strtab)[env_size] == NULL)
+		return ;
+	(*strtab)[env_size + 1] = NULL;
+}
+
 //export OK
 //Usage : Premier argument est un pointeur vers les tableau de chaines de caracteres env
 // Deuxieme argument un tableau NULL termine de chaines de caracteres NULL terminees listant les variables a ajouter;
 int    export(char **env[], char **arg)
 {
 	char	**env_cpy;
-	int		i;
+	char	*var_id;
+	char	*var;
 
-	i = 0;
 	if (*arg == NULL)
 	{
-		if (check_export_arg(*arg))
-			return;
-		while ((*env)[i])
-			i++;
-		env_cpy = (char **)malloc((i + 1) * sizeof(char *));
-		i = -1;
-		while ((*env)[++i])
-			env_cpy[i] = export_format((*env)[i]);
-		env_cpy[i] = NULL;
+		env_cpy = build_export_env(env);
 		print_export_env(env_cpy);
 		ft_free_tab(env_cpy);
 	}
 	else
 	{
-		replace_existing_vars(&arg, env);
-		add_strs_to_strtab(arg, env);
+		while (*arg)
+		{
+			var_id = NULL;
+			ft_printf("a\n");
+			if (check_export_arg(*arg))
+			{
+				ft_printf("Ma che\n");
+				return (1);
+			}
+			else
+			{
+				ft_printf("b\n");
+				var_id = extract_var_id(*arg);
+				ft_printf("c\n");
+			}
+			if (get_env_var(*env, var_id))
+			{
+				var = get_env_var(*env, var_id);
+				replace_var(env, ft_strdup(*arg), var);
+				var = NULL;
+			}
+			else
+				add_str_to_strtab(ft_strdup(*arg), env);
+			if (var_id)
+				free(var_id);
+			arg++;
+			// char *tmp = *(arg + 1);
+			// free(*arg);
+		}
 	}
 	return (0);
 }
