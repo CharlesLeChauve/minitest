292c6ac31de5c1d5d849c532d6dab92405f376de
CharlesLeChauve <teddy.gibert@gmail.com>
2024-07-03 15:17:43 +0200

    parsing norm, uncut

--- parsing.c
+++ parsing.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgibert <tgibert@student.42.fr>            +#+  +:+       +#+        */
+/*   And: anporced <anporced@student.42.fr>       +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 15:07:26 by tgibert           #+#    #+#             */
+/*   Updated: 2024/07/03 15:07:26 by tgibert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	incr_shlvl(char **var)
 {
 	int		i;
-	int 	lvl;
+	int		lvl;
 	char	*str_lvl;
 
 	i = 0;
 	while ((*var)[i])
 		i++;
 	i--;
-	while(ft_isdigit((*var)[i]))
+	while (ft_isdigit((*var)[i]))
 		i--;
 	i++;
 	lvl = ft_atoi(&(*var)[i]);
@@ -23,22 +35,22 @@ void	incr_shlvl(char **var)
 
 char	**set_env(char **envp)
 {
-	int     i;
+	int		i;
 	char	**env;
 
-    i = 0;
-    while (envp[i])
-        i++;
-    env = (char **)malloc(sizeof(char *) * (i + 1));
-    i = 0;
-    while (envp[i])
-    {
-        env[i] = ft_strdup(envp[i]);
+	i = 0;
+	while (envp[i])
+		i++;
+	env = (char **)malloc(sizeof(char *) * (i + 1));
+	i = 0;
+	while (envp[i])
+	{
+		env[i] = ft_strdup(envp[i]);
 		if (envp[i] == get_env_var(envp, "SHLVL"))
 			incr_shlvl(&env[i]);
-        i++;
-    }
-    env[i] = NULL;
+		i++;
+	}
+	env[i] = NULL;
 	return (env);
 }
 
@@ -56,10 +68,10 @@ char	*build_prompt(void)
 	return (prompt);
 }
 
-char *prompted_readline(void)
+char	*prompted_readline(void)
 {
+	char	*input;
 	//char *prompt;
-	char *input;
 
 	//prompt = build_prompt();
 	input = readline("minishell ");
@@ -69,7 +81,7 @@ char *prompted_readline(void)
 
 void	del_tkn_node(void *node_ptr)
 {
-	t_token_lst *node;
+	t_token_lst	*node;
 
 	if (!node_ptr)
 		return ;
@@ -95,7 +107,7 @@ void	free_ast(t_ast_node *ast)
 
 void	destroy_heredocs(void)
 {
-	int 	hd_1;
+	int		hd_1;
 	int		hd_2;
 	int		found;
 	char	path[15];
@@ -130,46 +142,6 @@ void	clean_shell_instance(t_shell *shl)
 	shl->token_lst = NULL;
 }
 
-int	ft_subshell(char *input, char *envp[])
-{
-	t_shell	shl;
-	int verif;
-
-	shl.env = set_env(envp);
-	shl.last_ret = 0;
-	shl.ast = NULL;
-	verif = 0;
-	if (input && *input)
-	{
-		shl.token_lst = tokenize(input);
-		verif = verify_tokens(shl.token_lst);
-		if (!verif)
-		{
-			ft_dlstclear(&(shl.token_lst), del_tkn_node);
-			clean_shell_instance(&shl);
-			free(input);
-			shl.last_ret = 2;
-			input = NULL;
-			ft_free_tab(shl.env);
-			return (2) ;
-		}
-		shl.ast = parse_tokens(shl.token_lst);
-		if (!shl.ast || expand_ast(shl.ast, &shl))
-		{
-			shl.last_ret = 2;
-			// free(input);
-			input = NULL;
-			clean_shell_instance(&shl);
-			ft_free_tab(shl.env);
-			return (2) ;
-		}
-		shl.last_ret = exec_ast(shl.ast, &shl);
-		clean_shell_instance(&shl);
-	}
-	ft_free_tab(shl.env);
-	return (shl.last_ret);
-}
-
 int	treat_input(char *input, t_shell *shl, int *verif)
 {
 	add_history(input);
@@ -194,23 +166,44 @@ int	treat_input(char *input, t_shell *shl, int *verif)
 	return (0);
 }
 
-int get_and_treat_input(t_shell *shl, int *verif)
+int	get_and_treat_input(t_shell *shl, int *verif)
 {
 	char	*input;
 
 	input = prompted_readline();
 	if (!input)
-		return (1) ;
+		return (1);
 	if (input && *input)
 	{
 		if (treat_input(input, shl, verif))
 		{
 			free(input);
-			return (2) ;
+			return (2);
 		}
 	}
 	free(input);
 	return (0);
+}
+
+int	ft_subshell(char *input, char *envp[])
+{
+	t_shell	shl;
+	int		verif;
+
+	shl.env = set_env(envp);
+	shl.last_ret = 0;
+	shl.ast = NULL;
+	verif = 0;
+	if (input && *input)
+	{
+		if (treat_input(input, &shl, &verif))
+		{
+			free(input);
+			return (2);
+		}
+	}
+	ft_free_tab(shl.env);
+	return (shl.last_ret);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -231,7 +224,7 @@ int	main(int argc, char *argv[], char *envp[])
 	{
 		status = get_and_treat_input(&shl, &verif);
 		if (status == 1)
-			break;
+			break ;
 		if (status == 2)
 			continue ;
 	}
